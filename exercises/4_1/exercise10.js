6120c49deb714b713a142b9d340103c8861da1e8
luizf.avelar <luiz.avelar25@gmail.com>
2020-05-12 19:58:20 -0300

    Fixed exercises 9; 10; 11 and add exercise 8

--- exercises/4_1/exercise10.js
+++ exercises/4_1/exercise10.js
@@ -1,8 +1,9 @@
-let cost_price = 2793
-let sale_price = 4324
-profit = 1000 * 0.8 * (sale_price - cost_price)
+let cost_price = 832
+let sale_price = 833
 
-if (sale_price > 0 && cost_price > 0){
+let profit = 1000 * 0.8 * (sale_price - cost_price)
+
+if (sale_price > 0 || cost_price > 0){
     console.log(profit)
 } else {
     console.log("Invalid entry")
--- exercises/4_1/exercise9.js
+++ exercises/4_1/exercise9.js
@@ -1,6 +1,6 @@
 let num1 = 32;
 let num2 = 16;
-let num3 = 44;
+let num3 = 43;
 
 if (num1 % 2 !== 0 || num2 % 2 !== 0 || num3 % 2 !== 0){
     console.log(true);
