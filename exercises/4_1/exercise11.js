6a3d6bf5d2f188ef3587c9a37ffcc874b43aa122
luizf.avelar <luiz.avelar25@gmail.com>
2020-05-12 18:56:41 -0300

    Exercises 9; 10 and 11

---
+++ exercises/4_1/exercise11.js
@@ -0,0 +1,29 @@
+let gross_salary = 3000;
+
+if (gross_salary <= 1556.95){
+    inss = gross_salary * 0.08;
+} else if (gross_salary >= 1556.95 && gross_salary <= 2594.82){
+    inss = gross_salary * 0.09;
+} else if (gross_salary >= 2594.93 && gross_salary <= 5189.82){
+    inss = gross_salary * 0.11;
+} else {
+    inss = 570.88;
+}
+
+base_salary = gross_salary - inss
+
+if (base_salary <= 1903.98){
+    ir = 0;
+} else if (base_salary >= 1903.99 && base_salary <= 2826.65){
+    ir = base_salary * 0.075 - 142.8;
+} else if (base_salary >= 2826.66 && base_salary <= 3751.05){
+    ir = base_salary * 0.15 - 354.8;
+} else if (base_salary >= 3751.06 && base_salary <= 4664.68){
+    ir = base_salary * 0.225 - 636.13;
+} else {
+    ir = base_salary * 0.275 - 869.36;
+}
+
+net_salary = base_salary - ir;
+
+console.log(net_salary)
---
+++ exercises/4_1/exercise9.js
@@ -0,0 +1,9 @@
+let num1 = 32;
+let num2 = 16;
+let num3 = 44;
+
+if (num1 % 2 !== 0 || num2 % 2 !== 0 || num3 % 2 !== 0){
+    console.log(true);
+} else{
+    console.log(false)
+}
