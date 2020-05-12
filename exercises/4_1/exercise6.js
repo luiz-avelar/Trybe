f1377a9cbf652ecd47585ae34620ba547865583f
luizf.avelar <luiz.avelar25@gmail.com>
2020-05-12 20:22:40 -0300

    Fixed exercise 6 again

--- exercises/4_1/exercise6.js
+++ exercises/4_1/exercise6.js
@@ -1,22 +1,22 @@
-let piece = "bishop";
+let piece = "knight";
 let piece_lowerCase = piece.toLowerCase();
 
 switch (piece_lowerCase){
     case "pawn":
-        alert("Moves vertically one or two squares")
+        console.log("Moves vertically one or two squares");
         break;
     case "bishop":
-        alert("Moves diagonally any number of squares")
+        console.log("Moves diagonally any number of squares");
         break;
     case "knight":
-        alert("Moves in L shape")
+        console.log("Moves in L shape");
         break;
     case "rook":
-        alert("Moves horizontally or vertically any number os squares")
+        console.log("Moves horizontally or vertically any number os squares");
         break;
     case "queen":
-        alert("Moves horizontally, vertically or diagonally any number of squares. But are unable to jump over pieces")
+        console.log("Moves horizontally, vertically or diagonally any number of squares. But is unable to jump over pieces");
         break;
     default:
-        alert("Moves horizontally, vertically or diagonally any number of squares and is able to jump over pieces");
+        console.log("Moves horizontally, vertically or diagonally any number of squares and is able to jump over pieces");
 }   
